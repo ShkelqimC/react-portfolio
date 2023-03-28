aa75d642541158dc4c86a2ca5ee26df73367bee4
Shkelqim Cakaj <shkelqimcakaj@hotmail.com>
2023-03-28 20:57:45 +0200

    .

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Navbar from "./components/nav/Nav";
+import Testimonials from "./components/testimonials/Testimonials";
+
+const App = () => {
+  return (
+    <>
+      <About />
+      <Contact />
+      <Experience />
+      <Footer />
+      <Header />
+      <Navbar />
+      <Testimonials />
+  
+    </>
+  );
+};
+
+export default App;
